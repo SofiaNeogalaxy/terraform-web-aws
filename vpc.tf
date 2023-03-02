a61549918541f6062aed4ba98708615988960bd4
SofiaNeogalaxy <114118855+SofiaNeogalaxy@users.noreply.github.com>
2023-03-02 16:05:37 -0600

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,44 @@
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Main"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "InternetGateway"
+  }
+}
+
+# Create a Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "Public"
+  }
+}
+
+# Create a Subnet
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Public"
+  }
+}
+
+# Associate the Route Table with the Subnet
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
