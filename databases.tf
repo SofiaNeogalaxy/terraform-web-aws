1871e9611d6fbe89f1edfd13036d0dcec7879bcb
SofiaNeogalaxy <114118855+SofiaNeogalaxy@users.noreply.github.com>
2023-03-02 16:07:51 -0600

    Create databases.tf

---
+++ databases.tf
@@ -0,0 +1,6 @@
+resource "aws_db_instance" "mysql" {
+  identifier                = "mysql-db"
+  engine                    = "mysql"
+  allocated_storage         = 20
+  instance_class            = "db.t2.micro"
+  name                     
